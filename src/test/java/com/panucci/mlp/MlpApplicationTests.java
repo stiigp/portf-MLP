10f9b966d096d058f09ca1def8a904ac24092ef1
stiigp <miltonfine33@gmail.com>
2026-08-03 18:26:53 -0300

    refactor: package name swap

---
+++ src/test/java/com/panucci/mlp/MlpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.panucci.mlp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MlpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
